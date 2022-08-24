c99c42919960dab03354a7d5144dddfa3bf96fd9
cindy9899 <yeonhoo9899@gmail.com>
2022-08-25 03:47:05 +0900

    Alert Component

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="show" class="app-alert alert" :class="styleClass" role="alert">
+    {{ message }}
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: 'error',
+    validator: value => ['success', 'error'].includes(value), //저 두개 말고 다른 타입이 넘어오면 error
+  },
+});
+
+const styleClass = computed(() =>
+  props.type === 'error' ? 'alert-danger' : 'alert-primary',
+);
+</script>
+
+<style scoped>
+.app-alert {
+  position: fixed;
+  top: 10px;
+  right: 10px;
+}
+</style>
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -18,6 +18,11 @@
         <button class="btn btn-primary">수정</button>
       </template>
     </PostForm>
+    <AppAlert
+      :show="showAlert"
+      :message="alertMessage"
+      :type="alertType"
+    ></AppAlert>
   </div>
 </template>
 
@@ -26,6 +31,7 @@ import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { getPostById, updatePost } from '@/api/posts';
 import PostForm from '@/components/posts/PostForm.vue';
+import AppAlert from '@/components/AppAlert.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -41,6 +47,7 @@ const fetchPost = async () => {
     setForm(data);
   } catch (error) {
     console.log(error);
+    vAlert('네트워크 오류@');
   }
 };
 const setForm = ({ title, content }) => {
@@ -52,13 +59,28 @@ fetchPost();
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value });
-    router.push({ name: 'PostDetail', params: { id } });
+    // router.push({ name: 'PostDetail', params: { id } });
+    console.log('hi');
+    vAlert('수정이 완료되었습니다.', 'success');
   } catch (error) {
     console.log(error);
   }
 };
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
+
+//alert
+const showAlert = ref(false);
+const alertMessage = ref('');
+const alertType = ref('');
+const vAlert = (message, type = 'error') => {
+  showAlert.value = true;
+  alertMessage.value = message;
+  alertType.value = type;
+  setTimeout(() => {
+    showAlert.value = false;
+  }, 2000);
+};
 </script>
 
 <style lang="scss" scoped></style>

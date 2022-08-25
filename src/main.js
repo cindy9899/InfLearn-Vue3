2daa6975779476260e9ab6638697a632fd851ff4
cindy9899 <yeonhoo9899@gmail.com>
2022-08-25 21:44:50 +0900

    dayjs for date format

--- src/main.js
+++ src/main.js
@@ -3,14 +3,13 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
-import objPlugins from '@/plugins/obj';
-import funcPlugins from '@/plugins/func';
-import person from '@/plugins/person';
-
+// import globalComponents from '@/plugins/global-components';
+import globalDirectives from '@/plugins/global-directives';
+import dayjs from '@/plugins/dayjs';
 const app = createApp(App);
-app.use(objPlugins, { name: '짐코딩' });
-app.use(funcPlugins);
-app.use(person, { name: '홍길동' });
+// app.use(globalComponents);
+app.use(globalDirectives);
+app.use(dayjs);
 app.use(router);
 app.mount('#app');
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,33 +7,17 @@
     <AppGrid :items="items" v-slot="{ item }" col-class="col-6">
       <AppCard>{{ item }}</AppCard>
     </AppGrid>
-    <hr class="my-4" />
-    <h2>{{ $person.name }}</h2>
-    <button class="btn btn-primary" @click="person.say">click person</button>
   </div>
 </template>
-<script>
-export default {
-  created() {
-    // console.log(this.$person.name);
-    this.$person.say();
-  },
-};
-</script>
 <script setup>
-import { inject, ref } from 'vue';
+import { ref } from 'vue';
 import router from '../router';
-import AppGrid from '@/components/AppGrid.vue';
-import AppCard from '@/components/AppCard.vue';
 
 const goAboutPage = () => {
   router.push('/about');
 };
 
 const items = ref(['사과', '딸기', '포도', '바나나']);
-
-const person = inject('person');
-console.log(person.name);
 </script>
 
 <style lang="scss" scoped></style>
